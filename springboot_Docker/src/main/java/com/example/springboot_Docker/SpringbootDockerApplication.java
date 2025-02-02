ad85952b7abb0d39bc8df9b632cef6edb3bcaa6b
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-02-02 20:03:16 +0630

    updating

---
+++ springboot_Docker/src/main/java/com/example/springboot_Docker/SpringbootDockerApplication.java
@@ -0,0 +1,21 @@
+package com.example.springboot_Docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerApplication {
+
+	@GetMapping("/getMessage")
+	public String getMessage(){
+		return "Welcome to Springboot Project with Dockerizing";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerApplication.class, args);
+	}
+
+}
